ae8155d24f428f4706c95384b3d27c656c7c3ac6
yoshi <yoshinic@icloud.com>
2020-04-27 17:23:06 +0900

    追加：Leaf を使用してブラウザ上からユーザー登録、登録したユーザーの表示
    [詳細]
    ・Public ディレクトリを追加。ここに独自ファイルを配置する
    ・Resources/View ディレクトリを追加。ここに .leaf ファイルを配置する
    
    [課題]
    ・ブログ投稿、表示
    ・タグ作成、表示

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -4,9 +4,54 @@ import Fluent
 struct WebsiteController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         routes.get(use: indexHandler)
+        routes.get("users", use: allUsersHandler)
+        routes.get("register", use: registerHandler)
+        routes.post("register", use: registerPostHandler)
     }
     
     func indexHandler(req: Request) throws -> EventLoopFuture<View> {
-        req.view.render("index", ["title": "Hi", "body": "Hello world!"])
+        req.view.render("index", IndexContext())
     }
+    
+    func allUsersHandler(_ req: Request) throws -> EventLoopFuture<View> {
+        User
+            .query(on: req.db)
+            .all()
+            .map { AllUsersContext(title: "ユーザー一覧", users: $0, userCounts: $0.count) }
+            .flatMap { req.view.render("allUsers", $0) }
+    }
+    
+    func registerHandler(_ req: Request) throws -> EventLoopFuture<View> {
+        let context: RegisterContext = .init()
+        return req.view.render("register", context)
+    }
+    
+    func registerPostHandler(_ req: Request) throws -> EventLoopFuture<Response> {
+        let data = try req.content.decode(RegisterPostData.self)
+        
+        let password = try Bcrypt.hash(data.password)
+        let user = User(name: data.name, username: data.username, passwordHash: password)
+        return user.save(on: req.db).map { user }.map { _ in req.redirect(to: "/") }
+    }
+}
+
+struct IndexContext: Encodable {
+    let title: String = "ブログトップ"
+}
+
+struct AllUsersContext: Encodable {
+    let title: String
+    let users: [User]
+    let userCounts: Int
+}
+
+struct RegisterContext: Encodable {
+    let title = "新規登録"
+}
+
+struct RegisterPostData: Decodable {
+    let name: String
+    let username: String
+    let password: String
+    let confirmPassword: String
 }
