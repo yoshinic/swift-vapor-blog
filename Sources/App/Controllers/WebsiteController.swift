2a1c882c348d9479229104f4a8b764bac1d8068b
yoshi <yoshinic@icloud.com>
2020-04-30 22:21:21 +0900

    追加：ログイン時にブログを作成できる機能追加
    [詳細]
    ・「ブログ作成」リンクからブログ作成ページに移動。
    ・ログインしていない場合は、ログインページにリダイレクトする
    ・ブログのタグ作成に Select2 ライブラリを使用。
    ・「ブログ作成」ページ読込み時に /api/tags にアクセスして保存している Tag を取得する
    ・ブログ保存時に Tag も保存する
    ・ログインしなくてもブログの閲覧はできる
    ・ユーザー一覧ページでユーザー名をクリックすると、そのユーザーが作成したブログ一覧が表示される
    
    [参照]
    select2
    https://select2.org/
    
    初期設定
    https://select2.org/tagging
    
    [課題]
    ブログの編集機能

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -6,13 +6,19 @@ struct WebsiteController: RouteCollection {
         let authSessionRoutes = routes.grouped(UserSessionAuthenticator())
         authSessionRoutes.get(use: indexHandler)
         authSessionRoutes.get("users", use: allUsersHandler)
+        authSessionRoutes.get("users", ":user_id", use: userHandler)
+        authSessionRoutes.get("blogs", ":blog_id", use: blogHandler)
         authSessionRoutes.get("register", use: registerHandler)
         authSessionRoutes.post("register", use: registerPostHandler)
         authSessionRoutes.post("logout", use: logoutHandler)
         authSessionRoutes.get("login", use: loginHandler)
         
-        let protectedRoutes = authSessionRoutes.grouped(UserCredentialsAuthenticator())
-        protectedRoutes.post("login", use: loginPostHandler)
+        let protectedRoutes = authSessionRoutes.grouped(User.redirectMiddleware(path: "/login"))
+        protectedRoutes.get("blogs", "create", use: createBlogHandler)
+        protectedRoutes.post("blogs", "create", use: createBlogPostHandler)
+        
+        let passwordProtected = authSessionRoutes.grouped(UserCredentialsAuthenticator())
+        passwordProtected.post("login", use: loginPostHandler)
     }
     
     func indexHandler(req: Request) throws -> EventLoopFuture<View> {
@@ -26,6 +32,37 @@ struct WebsiteController: RouteCollection {
             .all()
             .map { AllUsersContext(title: "ユーザー一覧", users: $0, userCounts: $0.count) }
             .flatMap { req.view.render("allUsers", $0) }
+    }
+    
+    func userHandler(_ req: Request) throws -> EventLoopFuture<View> {
+        User
+            .find(req.parameters.get("user_id"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { user in
+                user.$blogs.get(on: req.db).flatMap { (blogs: [Blog]) in
+                    let context: UserContext = .init(user: user, blogs: blogs)
+                    return req.view.render("user", context)
+                }
+        }
+    }
+    
+    func blogHandler(_ req: Request) throws -> EventLoopFuture<View> {
+        Blog
+            .find(req.parameters.get("blog_id"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { blog in
+                blog
+                    .$user
+                    .get(on: req.db)
+                    .and(blog.$tags.query(on: req.db).all())
+                    .flatMap { user, tags in
+                        let context = BlogContext(title: blog.title,
+                                                  blog: blog,
+                                                  user: user,
+                                                  tags: tags)
+                        return req.view.render("blog", context)
+                }
+        }
     }
     
     func loginHandler(_ req: Request) throws -> EventLoopFuture<View> {
@@ -60,6 +97,42 @@ struct WebsiteController: RouteCollection {
             return user.save(on: req.db).map { user }.map { _ in req.redirect(to: "/") }
         }
     }
+    
+    func createBlogHandler(_ req: Request) throws -> EventLoopFuture<View> {
+        let token = [UInt8].random(count: 16).base64
+        let context = CreateBlogContext(csrfToken: token)
+        req.session.data["CSRF_TOKEN"] = token
+        return req.view.render("createBlog", context)
+    }
+    
+    func createBlogPostHandler(_ req: Request) throws -> EventLoopFuture<Response> {
+        let data = try req.content.decode(CreateBlogData.self)
+        let expectedToken = req.session.data["CSRF_TOKEN"]
+        req.session.data["CSRF_TOKEN"] = nil
+        guard let csrfToken = data.csrfToken, expectedToken == csrfToken else {
+            throw Abort(.badRequest)
+        }
+        let userLoggedIn = try req.auth.require(User.self)
+        let blog = Blog(title: data.title,
+                        contents: data.contents,
+                        userID: try userLoggedIn.requireID())
+        return blog
+            .save(on: req.db)
+            .map{ blog }
+            .flatMapThrowing { savedBlog in
+                guard let savedBlogId = savedBlog.id else { throw Abort(.internalServerError) }
+                var tagSaves: [EventLoopFuture<Void>] = []
+                for tag in data.tags ?? [] {
+                    try tagSaves.append(
+                        Tag.addTag(tag, to: savedBlog, on: req))
+                }
+                return (tagSaves, savedBlogId)
+        }
+        .flatMap { (tagSaves: [EventLoopFuture<Void>], savedBlogId: Blog.IDValue) -> EventLoopFuture<Response> in
+            let redirect = req.redirect(to: "/blogs/\(savedBlogId)")
+            return tagSaves.flatten(on: req.eventLoop).transform(to: redirect)
+        }
+    }
 }
 
 struct IndexContext: Encodable {
@@ -71,6 +144,19 @@ struct AllUsersContext: Encodable {
     let title: String
     let users: [User]
     let userCounts: Int
+}
+
+struct UserContext: Encodable {
+    let title: String = "ユーザー情報"
+    let user: User
+    let blogs: [Blog]
+}
+
+struct BlogContext: Encodable {
+    let title: String
+    let blog: Blog
+    let user: User
+    let tags: [Tag]
 }
 
 struct LoginContext: Encodable {
@@ -96,4 +182,16 @@ struct RegisterPostData: Decodable {
     let username: String
     let password: String
     let confirmPassword: String
+}
+
+struct CreateBlogContext: Encodable {
+    let title = "ブログ作成"
+    let csrfToken: String
+}
+
+struct CreateBlogData: Decodable {
+    let title: String
+    let contents: String
+    let tags: [String]?
+    let csrfToken: String?
 }
